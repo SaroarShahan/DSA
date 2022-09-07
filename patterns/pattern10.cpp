e2b323ae29222113e26881131006c07610a4014e
saroarshahan <saroar.shahan@gmail.com>
2022-09-07 10:32:53 +0600

    add second approach for pattern ten

--- patterns/pattern10.cpp
+++ patterns/pattern10.cpp
@@ -5,9 +5,19 @@ int main() {
     int n;
     cin >> n;
 
+    /* Approach 1
     for(int row = 1; row <=n; row++) {
         for(int col = row; col >= 1; col--) {
             cout << col << " ";
+        }
+
+        cout << endl;
+    }
+    */
+
+    for(int row = 1; row <=n; row++) {
+        for(int col = 1; col <= row; col++) {
+            cout << row - col + 1 << " ";
         }
 
         cout << endl;
