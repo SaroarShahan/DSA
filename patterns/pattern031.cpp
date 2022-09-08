d6b3707db41c02f039a31ac1639f9b1e7623ac57
saroarshahan <saroar.shahan@gmail.com>
2022-09-08 18:15:06 +0600

    Update Butterfly patter's code

--- patterns/pattern031.cpp
+++ patterns/pattern031.cpp
@@ -5,36 +5,45 @@ int main() {
     int n;
     cin >> n;
 
-    for(int row = 1; row <=n; row++) {
+    int sp = 2 * (n - 1);
+    for(int row = 1; row <= n; row++) {
         for(int col= 1; col <= row; col++) {
             cout << "*";
         }
-        int sp = 2 * n - 2 * row;
+
         for(int space = 1; space <= sp; space++) {
             cout << " ";
         }
 
-        for(int col2 = 1; col2 <= row; col2++) {
-                cout << "*";
+        for(int col = 1; col <= row; col++) {
+            cout << "*";
         }
 
         cout << endl;
+
+        sp -= 2;
     }
 
-    for(int row = n - 1; row >= 1; row--) {
+    sp = 2;
+     for(int row = n -1; row >= 1; row--) {
         for(int col= 1; col <= row; col++) {
             cout << "*";
         }
-        int sp = 2 * n - 2 * row;
+
         for(int space = 1; space <= sp; space++) {
             cout << " ";
         }
-        for(int col2 = 1; col2 <= row; col2++) {
-                cout << "*";
+
+        for(int col = 1; col <= row; col++) {
+            cout << "*";
         }
 
         cout << endl;
+
+        sp += 2;
     }
+
+
 
     return 0;
 }
